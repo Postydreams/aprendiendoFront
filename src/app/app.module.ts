1b4ef4e0d2b0f658045db1028e3e3632097936dd
Esteban Ahumada <esteban.manuel.ahumada@gmail.com>
2022-12-20 20:12:29 -0300

    se creo y se configuro el archivo de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { MainComponent } from './componentes/main/main.component';
+import { TiempoComponent } from './componentes/tiempo/tiempo.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainComponent
+    MainComponent,
+    TiempoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
